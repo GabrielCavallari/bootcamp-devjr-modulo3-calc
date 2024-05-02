1c0dab646e1c55f5cb8c1c3edbb4f2c38b67e507
Gabriel Cavallari <gabriel.cavallari2003@gmail.com>
2024-05-02 14:59:59 -0300

    closes #3 Implementação do botão somar com js

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function calc(){
+    var num1 = parseFloat(document.getElementById("num1").value)
+    var num2 = parseFloat(document.getElementById("num2").value)
+    
+    output = num1 + num2
+
+    var output = document.getElementById("output").value = output
+
+}
