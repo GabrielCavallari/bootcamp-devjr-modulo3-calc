eb193211949623c880c7f3d926c8b9a25e705269
Gabriel Cavallari <gabriel.cavallari2003@gmail.com>
2024-05-13 13:27:22 -0300

    closes #4 - Implementar as 4 operações

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
-function calc(){
+function calc(op){
     var num1 = parseFloat(document.getElementById("num1").value)
     var num2 = parseFloat(document.getElementById("num2").value)
-    
-    output = num1 + num2
+    var output = 0;
 
-    var output = document.getElementById("output").value = output
+    switch(op) {
+        case '+' : output = num1 + num2; break;
+        case '-' : output = num1 - num2; break;
+        case '*' : output = num1 * num2; break;
+        case '/' : output = num1 / num2; break;
+    }
 
+    document.getElementById("output").value = output;
 }
